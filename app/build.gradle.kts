9212ac60ecb09fb752a2c5c55fa18249f6989830
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-12 21:08:32 +0800

    chore: clean duplicate dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
