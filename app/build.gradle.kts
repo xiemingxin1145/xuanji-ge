76ff71a9dd7288cc88db1e950cc9131925d4ae12
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-12 21:21:51 +0800

    chore: bump app version to 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aning.xuanxue"
         minSdk = 26
         targetSdk = 34
-        versionCode = 17
-        versionName = "1.7"
+        versionCode = 18
+        versionName = "1.8"
     }
 
     buildTypes {
