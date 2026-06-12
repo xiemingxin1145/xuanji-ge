f8812accc55edd9ccdc80cfa0c40a094fa0cb73c
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-12 20:45:05 +0800

    chore: add app version constants

---
+++ app/src/main/java/com/aning/xuanxue/AppVersion.kt
@@ -0,0 +1,8 @@
+package com.aning.xuanxue
+
+object AppVersion {
+    const val VERSION_NAME = "1.2"
+    const val VERSION_CODE = 12
+    const val EDITION = "问玄增强预备版"
+    const val DISPLAY = "玄机阁 v1.2 · 问玄增强预备版"
+}
