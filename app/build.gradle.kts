cdf88f7a93fb022207deacc1d56ad75ddd78317c
xiemingxin1145 <gayakethanda@gmail.com>
2026-06-12 20:52:51 +0800

    chore: bump app version to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aning.xuanxue"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
     }
 
     buildTypes {
